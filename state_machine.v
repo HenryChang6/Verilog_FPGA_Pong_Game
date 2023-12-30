af99328f64376a9617e21cb1c428308981b01654
HenryChang6 <henry930619@gmail.com>
2023-12-30 23:45:36 +0800

    delete paddle_x

---
+++ state_machine.v
@@ -0,0 +1,20 @@
+module state_machine(
+    input  clk,
+    input  rst,
+    input  start,
+    input  up1,
+    input  up2,
+    input  down1,
+    input  down2,
+    input  sec1, // ???
+    output ball_x,
+    output ball_y,
+    output paddle1_x,
+    output paddle1_y,
+    output paddle2_x,
+    output paddle2_y,
+ );
+
+ 
+
+endmodule
