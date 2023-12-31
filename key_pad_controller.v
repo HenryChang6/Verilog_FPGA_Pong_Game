79a62291ef0bee49d2a484972495773ffb6810fe
HenryChang6 <henry930619@gmail.com>
2023-12-31 20:37:28 +0800

    tmp

---
+++ key_pad_controller.v
@@ -0,0 +1,14 @@
+module key_pad_controller (
+    input clk, //100Hz
+    input rst,
+    input [3:0] kp_row,
+    input [3:0] kp_col, 
+    output up1, // A being pressed
+    output up2, // 8 being pressed
+    output down1, // 0 being pressed
+    output down2, // 7 being pressed
+);
+
+
+
+endmodule
