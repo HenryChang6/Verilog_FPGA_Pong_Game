9af4e4e31399acd8f49ab728c16d76967a10596b
HenryChang6 <henry930619@gmail.com>
2023-12-31 17:48:08 +0800

    fd_ball complete

---
+++ fd_ball.v
@@ -0,0 +1,15 @@
+module fd_ball(
+    input      clk,  // 50MHz input clk
+    input      rst,
+    output reg clk_25MHz // 25MHz
+);
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        clk_25MHz <= 0;
+    end else begin
+        clk_25MHz <= ~clk_25MHz;  // 在每個輸入時鐘的上升沿翻轉輸出時鐘的狀態
+    end
+end
+
+endmodule
